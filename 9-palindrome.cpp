e5bf7b2c38c5aea8c6e1490d744cace9564b0e17
Anvesha Khandelwal <anveshak0906@gmail.com>
2025-11-10 18:54:07 +0530

    added

--- 9-palindrome.cpp
+++ 9-palindrome.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers and numbers ending with 0 (except 0 itself) cannot be palindromes
+       
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
         int original = x;
-        long long ans = 0; // use long long to avoid overflow
+        long long ans = 0; 
         int digit;
 
         while (x != 0) {
