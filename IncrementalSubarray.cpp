d636d5a5d08d7bd6fd487551fdd14ef963ff6634
Anvesha Khandelwal <anveshak0906@gmail.com>
2025-11-12 21:03:26 +0530

    added

--- IncrementalSubarray.cpp
+++ IncrementalSubarray.cpp
@@ -1,11 +1,36 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int T;
-    cin>>T;
-    while(T--){
-        
 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        long long n, m;
+        cin >> n >> m;
+        vector<long long> a(m);
+        for(int i = 0; i < m; i++){
+            cin >> a[i];
+        }
+
+        bool hasNonIncreasing = false;
+        for(int i = 0; i + 1 < m; i++){
+            if(a[i] >= a[i+1]){
+                hasNonIncreasing = true;
+                break;
+            }
+        }
+
+        if(hasNonIncreasing){
+            
+            cout << 1 << "\n";
+        } else {
+            long long r = a[m-1];
+            cout << (n - r + 1) << "\n";
+        }
     }
+
     return 0;
 }
